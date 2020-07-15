26f4c1a8d60c6e9e4dc5e92f956956e349010c89
sahir2008 <55133035+sahir2008@users.noreply.github.com>
2020-07-15 14:33:28 +0200

    add dice game files

---
+++ index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+document.querySelector("img").setAttribute("src", "images/dice"+randomNumber1+".png");
+document.querySelector("img.img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "🎈Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+	document.querySelector("h1").innerHTML = "Player 2 Wins!🚬";
+}
+else{
+	document.querySelector("h1").innerHTML = "Draw";
+}
+
+}
